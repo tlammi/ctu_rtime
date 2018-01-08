c4373f5d11983747ec986d3e1c981ee38f5d5b5f
lammiton <lammiton@k09-207.felk.cvut.cz>
2018-01-08 18:06:58 +0100

    TCP server now reads data from fifo buffer
    Added functionality for reading fifo buffer for
    wanted data and pushing it to TCP interface.
    
    Added comments.

--- semestralWork/tcpServerTask/tcpServerTask.c
+++ semestralWork/tcpServerTask/tcpServerTask.c
@@ -1,49 +1,65 @@
 #include "tcpServerTask.h"
 #include "graphInterfaceTypes.h"
 #include "TCPInterface.h"
+#include "fifoBuffer.h"
+#include "priorities.h"
+
 
 #include <stdlib.h>
 #include <taskLib.h>
 #include <kernelLib.h>
 
 
-static void graphUpdaterTask(){
+//! Read data from fifo buffer and push it to TCP interface
+static void graphUpdaterTask(FifoHandl fifoHandl){
 	while(1){
+		
+		// Read data from buffer. This buffer call is blocking.
+		int actVal, reqVal, pwmVal;
+		fifo_pop(fifoHandl, &actVal);
+		fifo_pop(fifoHandl, &reqVal);
+		fifo_pop(fifoHandl, &pwmVal);
+		
 		GraphData data;
-		data.actPos = rand();
-		data.pwmDuty = rand();
-		data.reqPos = rand();
-		printf("%d\n",data.actPos);
+		data.actPos = actVal;
+		data.pwmDuty = pwmVal;
+		data.reqPos = reqVal;
+		
+		// Push data to TCP server interface
 		TCP_pushGraphData(data);
-		taskDelay(500);
 	}
 }
 
-void tcpServerTask(){
+void tcpServerTask(FifoHandl fifoHandl){
 	
-	srand(0);
+	// Spawn a task for reading fifo buffer with data coming from motor writer.
+	// Priority is higher than that of server tasks
+	taskSpawn("graphUpdater", PRIORITY_TCP_SERVER-1, 0, 4096, (FUNCPTR) 	graphUpdaterTask, fifoHandl,0,0,0,0,0,0,0,0,0);
 	
-	taskSpawn("graphUpdater", 210, 0, 4096, (FUNCPTR) 	graphUpdaterTask, 0,0,0,0,0,0,0,0,0,0);
-	
+	// Initialize TCP server socket
 	TCPHandle handl = TCP_init();
 	while(handl == -1){
 		handl = TCP_init();
 		taskDelay(500);
 	}
+	
+	// Listen to TCP queries and answer to them
 	TCPHandle cliHandl;
 	
 	while(1){
+		// Get a new connection
 		cliHandl = TCP_listen(handl);
-		taskDelay(100);
 		if(cliHandl == -1){
 			perror("TCP_listen failed");
 		}
 		else{
+			// Answer to client
 			if(TCP_answerToClient(cliHandl) != 0){
 				perror("TCP_answerToClient failed");
 			}
 			
 		}
+		// Close connection to client
 		TCP_closeInterface(cliHandl);
 	}
 }
